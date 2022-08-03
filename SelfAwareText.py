87973d5a4ac711f53b48e858bb2f02fa63d5dc99
pogicraft <93170796+pogicraft@users.noreply.github.com>
2022-08-02 20:06:43 -0400

    Update SelfAwareText.py
    changed static numbers into variables and reorganized things so that the reimplemented functions are all under the custom functions

--- SelfAwareText.py
+++ SelfAwareText.py
@@ -10,7 +10,7 @@ class AppearObject(QGraphicsObject):
 		super().__init__(parent)
 		
 		self.font = self.set_font(font)
-		self.content = text
+		self.content = str(text)
 		self.font_measure = QFontMetrics(self.font)
 		self.show = True
 		self.margins = QMargins(5, 5, 5, 5)
@@ -18,11 +18,36 @@ class AppearObject(QGraphicsObject):
 		self.border_pen = QPen(Qt.blue, 3, Qt.DashLine)
 		self.text_options = QTextOption()
 		self.text_options.setAlignment(Qt.AlignCenter)
+		self.max_width = self.font_measure.width(self.content, Qt.AlignCenter)
+		self.lines = 1
+		self.line_height = self.font_measure.lineSpacing()
 		
+	def toggle_visibility(self):
+		if self.show:
+			self.show = False
+		else:
+			self.show = True
+		return self
+		
+	def set_text(self, string):
+		self.content = str(string)
+		
+	def set_font(self, font):
+		if type(font) == QFont:
+			return font
+		elif type(font) == tuple:
+			return QFont(font)
+		else:
+			return self.parent().font()
+			
+	def set_width(self, integer):
+		self.max_width = integer
+		self.lines = self.font_measure.width(self.content) / integer
+	
 	def boundingRect(self):
 		c = self.mapToParent(0, 0)
 		if self.show:
-			return QRect(c.x(), c.y(), self.font_measure.width(self.content, Qt.AlignCenter)+self.margins.left()+self.margins.right(), self.font_measure.height()+self.margins.top()+self.margins.bottom())
+			return QRect(c.x(), c.y(), self.max_width+self.margins.left()+self.margins.right(), self.line_height*self.lines+self.margins.top()+self.margins.bottom())
 		else:
 			return QRect(c.x(), c.y(), 0, 0)
 	
@@ -35,25 +60,7 @@ class AppearObject(QGraphicsObject):
 			painter.setPen(self.border_pen)
 			painter.drawRect(self.boundingRect())
 		painter.restore()
-	
-	def toggle_visibility(self):
-		if self.show:
-			self.show = False
-		else:
-			self.show = True
-		return self
-			
-	def set_text(self, string):
-		self.content = str(string)
 		
-	def set_font(self, font):
-		if type(font) == QFont:
-			return font
-		elif type(font) == tuple:
-			return QFont(font)
-		else:
-			return self.parent().font()
-			
 	def mousePressEvent(self, event):
 		self.clicked.emit(self)
 		super().mousePressEvent(event)
